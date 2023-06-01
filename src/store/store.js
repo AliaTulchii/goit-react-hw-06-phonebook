f6e2091a35090ea7066995a1803a28fc47ee3194
Halina Tulchii <halendeihalyna@gmail.com>
2023-06-01 13:20:51 +0300

    update

--- src/store/store.js
+++ src/store/store.js
@@ -6,10 +6,19 @@ import contactSlice from "features/contactSlice";
 const persistConfig = {
     key: 'root',
     storage,
-  }
+}
+
+const persistedContactsReducer = persistReducer(
+    persistConfig,
+    contactSlice
+)
 
 export const store = configureStore({
     reducer: {
-        contact: contactSlice,
+        // contact: contactSlice,
+        contact: persistedContactsReducer,
     },
-})
+});
+
+export const persistor = persistStore(store);
+
